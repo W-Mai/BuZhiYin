bac3652f867f46d562b64865631c3139361189e4
W-Mai <1341398182@qq.com>
2023-03-28 12:31:56 +0800

    更新友情链接格式

--- Utils/Utils.swift
+++ Utils/Utils.swift
@@ -23,6 +23,6 @@ func clamp<T: Comparable>(_ value: T, lowerBound: T, upperBound: T) -> T {
 
 func friendLinks() -> [FriendLinkInfo]{
     return [
-        FriendLinkInfo(icon: "MenubarX", title: "MenubarX", desc: "一款强大的菜单栏浏览器", link: URL(string: "https://apps.apple.com/cn/app/id1575588022?mt=8&ct=app")!),
+        FriendLinkInfo(icon: "MenubarX", title: "MenubarX", desc: "一款强大的菜单栏浏览器", link: URL(string: "macappstore://apps.apple.com/cn/app/id1575588022?mt=8&ct=app")!),
     ]
 }
